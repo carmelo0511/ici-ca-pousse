50ed5f6e7a668d2e63869dcb51ded0984b1bf8d5
Bryan Nakache <nakachebryan@gmail.com>
2025-07-28 19:25:57 -0400

    Fix remaining ESLint errors - remove unused imports

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../utils/firebase';
